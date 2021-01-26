d0589c53f49c869ceaf376736fe646ca392f630f
Victor <darcel.v@gmail.com>
2021-01-26 19:51:56 +0100

    add button in header

--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,4 @@
-//import { Link } from "gatsby"
+import { Link } from "gatsby"
 //import PropTypes from "prop-types"
 import React from "react"
 import Icon from "../images/svg/icon.inline.svg"
@@ -8,8 +8,10 @@ import { layout, media, pxToRem } from "../theme/helpers"
 const Header = ({ className }) => (
   <header className={className}>
     <div>
-      <Icon />
-      <p>Pour une installation réussie et sans souci !</p>
+      <Link to="/">
+        <Icon />
+        <p>Pour une installation réussie et sans souci !</p>
+      </Link>
     </div>
   </header>
 )
@@ -21,30 +23,35 @@ export default styled(Header)`
   align-items: center;
   width: 100%;
   div {
-    display: flex;
     width: 100%;
-    margin: ${pxToRem(10)};
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    p {
-      text-align: center;
-      align-self: center;
-      font-size: ${pxToRem(18)};
-      margin-top: ${pxToRem(10)};
+    a {
+      display: flex;
+      margin: ${pxToRem(10)};
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      text-decoration: none;
+      p {
+        text-align: center;
+        align-self: center;
+        font-size: ${pxToRem(18)};
+        margin-top: ${pxToRem(10)};
+      }
     }
   }
 
   ${media.medium`
   max-height: ${pxToRem(90)};
   div {
-  flex-direction: row;
-  justify-content: flex-start;
-  p{
-    align-self:flex-end;
-    font-size: ${pxToRem(20)};
-    line-height: 1;
-  }
+    a{
+    flex-direction: row;
+    justify-content: flex-start;
+      p{
+        align-self:flex-end;
+        font-size: ${pxToRem(20)};
+        line-height: 1;
+      }
+    }
   }
   `}
 `
