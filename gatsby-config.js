2fb60b160a09532a38c164b0c03b2ac7ad0d09a4
Victor <darcel.v@gmail.com>
2021-01-28 19:44:56 +0100

    fav icon and theme color

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,13 +18,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Mon-eco-habitat`,
+        short_name: `eco-habitat`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#0fd344`,
+        theme_color: `#0fd344`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon-mon-eco-habitat.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-styled-components`,
